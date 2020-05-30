87fc9451f7c4d6f136698082fe43cc422b5dc796
Oisin McLaughlin <oisin_mcl_@hotmail.com>
2020-05-30 18:16:09 +0100

    Added Common CSS and Colour Picker

--- src/js/content.js
+++ src/js/content.js
@@ -1,6 +1,6 @@
 // var bkg = chrome.extension.getBackgroundPage();
 // bkg.console.log('content.js');
-console.log('content.js');
+console.log('Entered content.js');
 
 function setup (){
 
--- src/js/popup.js
+++ src/js/popup.js
@@ -1,37 +1,15 @@
-let changeColor = document.getElementById('changeColor');
 var bkg = chrome.extension.getBackgroundPage();
 
-chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
-});
+colorPicker.addEventListener("change", watchColorPicker, false);
 
-changeColor.onclick = function(element) {
-
-    chrome.storage.sync.get('color', function(data) {
-    let color = data.color;
+function watchColorPicker(event) {
+  let color = event.target.value;
+  chrome.storage.sync.set({color: event.target.value}, function() {
+    bkg.console.log('color is ' + event.target.value);
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
       chrome.tabs.executeScript(
           tabs[0].id,
           {code: 'document.body.style.background = "' + color + '";'});
     });
-    bkg.console.log('Background colour changed to: ' + color);
-  });
-};
-  
-let page = document.getElementById('buttonDiv');
-const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
-function constructOptions(kButtonColors) {
-  for (let item of kButtonColors) {
-    let button = document.createElement('button');
-    button.style.backgroundColor = item;
-    button.addEventListener('click', function() {
-      chrome.storage.sync.set({color: item}, function() {
-        bkg.console.log('Selected color changed to: ' + item);
-      })
-      changeColor.style.backgroundColor = item;
-    });
-    page.appendChild(button);
-  }
+  })
 }
-constructOptions(kButtonColors);
