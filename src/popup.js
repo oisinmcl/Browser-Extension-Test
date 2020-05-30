f0a1afd88f95c9484873a7fdfb9c7f587f4e2633
Oisin McLaughlin <oisin_mcl_@hotmail.com>
2020-05-30 13:40:25 +0100

    Added colour buttons to popup

--- src/popup.js
+++ src/popup.js
@@ -6,11 +6,29 @@ chrome.storage.sync.get('color', function(data) {
 });
 
 changeColor.onclick = function(element) {
-    let color = element.target.value;
+    chrome.storage.sync.get('color', function(data) {
+    let color = data.color;
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
       chrome.tabs.executeScript(
           tabs[0].id,
-          {code: 'document.body.style.backgroundColor = "' + color + '";'});
+          {code: 'document.body.style.background = "' + color + '";'});
     });
-  };
+  });
+};
   
+let page = document.getElementById('buttonDiv');
+const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
+function constructOptions(kButtonColors) {
+  for (let item of kButtonColors) {
+    let button = document.createElement('button');
+    button.style.backgroundColor = item;
+    button.addEventListener('click', function() {
+      chrome.storage.sync.set({color: item}, function() {
+        console.log('color is ' + item);
+      })
+      changeColor.style.backgroundColor = item;
+    });
+    page.appendChild(button);
+  }
+}
+constructOptions(kButtonColors);
